5ce1bfed9e0b83964c33e7d912e4610ff65dfb93
John Reid <johnmicahreid@gmail.com>
2017-02-07 10:55:09 +0800

    Test shots from a new camera script

--- camera.py
+++ camera.py
@@ -1,12 +1,36 @@
+from picamera.array import PiRGBArray
 from picamera import PiCamera
-from time import sleep
+import time
+import datetime
+import cv2
 
+# Set up the camera
 camera = PiCamera()
-
-camera.resolution = (800, 400)
 camera.framerate = 15
+camera.resolution = (640, 480)
+camera.hflip = True
+camera.vflip = True
+rawCapture=PiRGBArray(camera, size=(640, 480))
+time.sleep(0.1)
 
-camera.start_preview(alpha=200)
-sleep(10)
-camera.capture('/home/pi/Desktop/test.jpg') 
-camera.stop_preview()
+shot_number = 0
+
+# Get the image
+for frame in camera.capture_continuous(rawCapture, format="bgr",
+                                       use_video_port=True):
+
+    image=frame.array
+
+    cv2.imshow("Frame", image)
+    key=cv2.waitKey(1) & 0xFF
+
+    rawCapture.truncate(0)
+
+    if key == ord("q"):
+        break
+
+    if key == ord("t"):
+        camera.capture('/home/pi/capstone/pics/PIC%s.jpg' % \
+                       str(datetime.datetime.now()))
+        shot_number += 1
+
